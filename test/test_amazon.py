a87331b31d9a41b3589b5c7b118bfc5588132ea9
ch <chaithanya319@outlook.com>
2021-05-10 16:15:13 +0530

    trails

--- test/test_amazon.py
+++ test/test_amazon.py
@@ -2,7 +2,7 @@ from utility.driverutility import Driverutility
 import pytest
 
 
-class Testgoogle(Driverutility):
+class Testamazon(Driverutility):
     @pytest.mark.regression
     def test_chrome(self):
         headless_status = super().parse_config("project_config", "browser_config", "headless_status")
@@ -18,6 +18,6 @@ class Testgoogle(Driverutility):
         headless_status = super().parse_config("project_config", "browser_config", "headless_status")
         driver = super().spawn_driver("chrome", headless_status)
         driver.get("https://www.amazon.in/")
-        assert driver.title == "nline Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in", "Title does not match"
+        assert driver.title == "Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in", "Title does not match"
 
         super().tear_down(driver)
